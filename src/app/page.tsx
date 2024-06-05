cd0a72e292899e5fcea9191b2606a14a8f70e93d
Jmerc03 <jaxmerc03@gmail.com>
2024-06-05 10:37:49 -0500

    UI mark 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,13 @@ import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
-// import "./page.css";
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
-// import { get } from "http";
+import { Hidden } from "@mui/material";
 
-const serial: string[] = ["00M09A342600271",
-"00M09A342600260",
-"00M09C431500177",
-"00M09C431903549",
-"00M09C431903109",
-"00M09C411300736",
-"00M09C412401291",
-"00M09A381601473",
-"00M00A2C0603600",
-"00M09A381800295",
-"00M09A351803089",
-"00M09A381800295",
-"00M09C431501094"];
+const serial: string[] = ["00M09A342600271", "00M09A342600260", "00M09C431500177", 
+"00M09C431903549", "00M09C431903109", "00M09C411300736", "00M09C412401291", "00M09A381800295",
+"00M09A381601473", "00M00A2C0603600","00M09A381800295", "00M09A351803089",  "00M09C431501094"];
 
 class Printer {
   public id: string;
@@ -66,20 +55,10 @@ const update = (socketData:any, device:Printer, serialNum:string) => {
 
 var printerArray: Printer[] = [];
 
-const setPrinterArray = () => {
-  for(let i =0; i<serial.length; i++){
-    printerArray[i] = new Printer(serial[i]);
-  }
-};
-
 const updatePrinterArray = (socketData:any) => {
   for(let i =0; i<serial.length; i++){
-    // console.log("serial:",serial[i]);
-    // console.log("printer:", (JSON.stringify((socketData?.printer?.id), replacer, 2).replaceAll( '\"', '')));
     if(serial[i] == (JSON.stringify((socketData?.printer?.id), replacer, 2).replaceAll( '\"', ''))){
-      // console.log(socketData);
       printerArray[i] = update(socketData , printerArray[i], serial[i]);
-      // console.log(printerArray);
       break;
     }
   }
@@ -88,12 +67,12 @@ const updatePrinterArray = (socketData:any) => {
 
 function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
+    <Box sx={{ display: 'flex', alignItems: 'center', marginRight:1}}>
+      <Box sx={{ width: '100%', mr: 1}}>
+        <LinearProgress className="loadingBar" sx={{height: 20}} variant="determinate" {...props} />
       </Box>
       <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${props.value}%`}</Typography>
+        <Typography variant="body2" color="white" fontSize={20}>{`${props.value}%`}</Typography>
       </Box>
     </Box>
   );
@@ -141,47 +120,50 @@ export default function Home() {
   React.useEffect(() => {
     updatePrinterArray(socketData);
   },[socketData]);
-  console.log(socketData);
+  //console.log(socketData);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Box>
-        <Grid>
-          {Array.from(Array(1)).map((_, index) => (
-            <Grid>
-              {printerArray.map(printerArray => 
-                <div id={index.toString()} className={`main-${printerArray?.error}`}>
+      <Box sx={{flexGrow:1}}>
+        <Grid margin={0} container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {printerArray.map(printerArray => 
+            <Grid xs={2} sm={4} md={4} >
+                <div id={`main-${printerArray?.error}`} className="box">
+                  <div className="innerContainer">
+                    <div className="title">
+                      <p>{printerArray.name}</p>
+                    </div>
+                    <p>Current Print :{printerArray.printJob}</p>
 
-                  <p>{printerArray.name}</p>
-                  <p>{printerArray.printJob}</p>
-                  <p>Time Remaining: {printerArray.timeLeft}</p>
-                  <Box sx={{ width: '100%' }}>
-                    <LinearProgressWithLabel value={printerArray.percent} />
-                  </Box>
 
-                  <p>Active Filament: {printerArray.filName}</p>
+                    <p>Active Filament: {printerArray.filName}</p>
 
-                  {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[0].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[0].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
-                  {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[1].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[1].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
-                  {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[2].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[2].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
-                  {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
-                  <p>{printerArray?.error === '0' ? 'OK' : 'ERROR' }</p>
-                  {/* <p id="1234" >Status: {getError(printerArray.error)}</p>  */}
-                  {/* <p>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].name), replacer, 2).replaceAll( '\"', ''))}</p> */}
-                  <p>{printerArray.temp}</p>
+                    {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[0].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[0].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
+                    {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[1].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[1].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
+                    {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[2].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[2].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
+                    {/* <p style={{color:"#".concat((JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].tray_color), replacer, 2).replaceAll( '\"', '')))}}>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].name), replacer, 2).replaceAll( '\"', ''))} </p> */}
+                    <p>Status: {printerArray?.error === '0' ? 'OK' : (printerArray?.error === "117473284") ? "Spagetti" : "ERROR CODE: " + printerArray?.error }</p>
+                    {/* <p id="1234" >Status: {getError(printerArray.error)}</p>  */}
+                    {/* <p>{(JSON.stringify((socketData?.data?.ams?.ams[0].tray[3].name), replacer, 2).replaceAll( '\"', ''))}</p> */}
+                    <div className="time">
+                      <p>Time Remaining:</p><p style={{fontSize:20, paddingLeft:4}}>{printerArray.timeLeft}</p>
+                    </div>
+                    <Box sx={{ width: '100%' }}>
+                      <LinearProgressWithLabel value={printerArray.percent} />
+                    </Box>
+                  </div>
                 </div> 
-              )}
             </Grid>
-          ))}
+          )}
         </Grid>
       </Box>
       <div>
-        {/* <p>{printerArray[0].id}</p> */}
+        {/* <p>{printerArray[0].id}</p>
         <h1>Device socketData</h1>
         {socketData ? (
           <pre>{JSON.stringify(socketData, null, 100)}</pre>
         ) : (
           <p>No socketData received yet...</p>
-        )}
+        )} */}
       </div>
     </main>
 
